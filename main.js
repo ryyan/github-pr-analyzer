bb420f9acf9b619680c40c41299cc71fdb63bea1
ryyan <ryyanrb@gmail.com>
2017-12-19 13:34:43 -0600

    Split getRepositories function from getPRs

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const https = require('https');
 const config = require('./config.json');
 
+class Repository {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.PullRequests = [];
+  }
+}
+
+class PullRequest {
+  constructor(id, title, body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+}
+
 class Github {
   constructor(github_token) {
-    this.options = {
+    const options = {
       hostname: 'api.github.com',
       path: '/graphql',
       method: 'POST',
@@ -13,60 +29,106 @@ class Github {
         'Content-Type': 'application/json'
       }
     };
+
+    this.request = (query) => {
+      return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+          let result = ''
+
+          res.setEncoding('utf8');
+
+          res.on('data', (chunk) => {
+            result += chunk
+          });
+
+          res.on('error', (err) => {
+            reject(e.message);
+          });
+
+          res.on('end', () => {
+            resolve(JSON.parse(result));
+          });
+        });
+
+        req.write(JSON.stringify({query}));
+        req.end();
+      });
+    };
   }
 
-  getPRs(github_organization) {
-    return new Promise((resolve, reject) => {
-      const req = https.request(this.options, (res) => {
-        let result = ''
-
-        res.setEncoding('utf8');
-
-        res.on('data', (chunk) => {
-          result += chunk
-        });
-
-        res.on('error', (err) => {
-          reject(e.message);
-        });
-
-        res.on('end', () => {
-          resolve(JSON.parse(result));
-        });
-      });
-
+  async getRepositories(owner) {
+    try {
       const query = `query {
-        organization(login:"${github_organization}") {
-          repositories(first:10) {
+        organization(login: "${owner}") {
+          repositories(first:100) {
             edges {
               node {
                 id
                 name
-                pullRequests(first:10) {
-                  edges {
-                    node {
-                      id
-                      title
-                    }
-                  }
-                }
+                nameWithOwner
               }
             }
           }
         }
       }`;
 
-      req.write(JSON.stringify({query}));
-      req.end();
-    });
+      let result = await this.request(query);
+      return result['data']['organization']['repositories']['edges']
+        .map(repository => Object.assign(new Repository, repository['node']));
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async getPRs(repository_owner, repository_name) {
+    try {
+      const query = `query {
+        repository(owner: "${repository_owner}", name: "${repository_name}") {
+          pullRequests(first:100) {
+            edges {
+              node {
+                id
+                number
+                title
+                body
+                author {
+                  login
+                }
+                baseRefName
+                additions
+                deletions
+                changedFiles
+                state
+                createdAt
+                closed
+                closedAt
+                merged
+                mergedAt
+                mergeable
+              }
+            }
+          }
+        }
+      }`;
+
+      let result = await this.request(query);
+      console.log(JSON.stringify(result, null, 4));
+      return result['data']['repository']['pullRequests']['edges']
+        .map(pr => Object.assign(new PullRequest, pr['node']));
+    } catch (e) {
+      throw e;
+    }
   }
 }
 
 async function main() {
   try {
     let github = new Github(config.github_token);
-    let prs = await github.getPRs(config.github_organization);
-    console.log(JSON.stringify(prs, null, 4));
+    let repositories = await github.getRepositories(config.github_organization);
+    console.log(repositories);
+
+    let prs = await github.getPRs(config.github_organization, repositories[0].name);
+    console.log(prs);
   } catch (e) {
     console.error(e);
   }
