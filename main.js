03dfcbaf8708e1934f2b4f8dc69f6ab64bf443fd
ryyan <ryyanrb@gmail.com>
2017-12-19 10:30:07 -0600

    Create Github class

--- main.js
+++ main.js
@@ -1,59 +1,75 @@
 const https = require('https');
 const config = require('./config.json');
 
-function main() {
-  const options = {
-    hostname: 'api.github.com',
-    path: '/graphql',
-    method: 'POST',
-    headers: {
-      'Authorization': `token ${config.github_token}`,
-      'User-Agent': 'github-pr-analyzer',
-      'Content-Type': 'application/json'
-    }
-  };
+class Github {
+  constructor(github_token) {
+    this.options = {
+      hostname: 'api.github.com',
+      path: '/graphql',
+      method: 'POST',
+      headers: {
+        'Authorization': `token ${github_token}`,
+        'User-Agent': 'github-pr-analyzer',
+        'Content-Type': 'application/json'
+      }
+    };
+  }
 
-  const req = https.request(options, (res) => {
-    let result = ''
+  getPRs(github_organization) {
+    return new Promise((resolve, reject) => {
+      const req = https.request(this.options, (res) => {
+        let result = ''
 
-    res.setEncoding('utf8');
+        res.setEncoding('utf8');
 
-    res.on('data', (chunk) => {
-      result += chunk
-    });
+        res.on('data', (chunk) => {
+          result += chunk
+        });
 
-    res.on('error', (err) => {
-      console.error(`${e.message}`);
-    });
+        res.on('error', (err) => {
+          reject(e.message);
+        });
 
-    res.on('end', () => {
-      console.log(JSON.stringify(JSON.parse(result), 0, 4));
-    });
-  });
+        res.on('end', () => {
+          resolve(JSON.parse(result));
+        });
+      });
 
-  const query = `query {
-    organization(login:"${config.github_organization}") {
-      repositories(first:10) {
-        edges {
-          node {
-            id
-            name
-            pullRequests(first:10) {
-              edges {
-                node {
-                  id
-                  title
+      const query = `query {
+        organization(login:"${github_organization}") {
+          repositories(first:10) {
+            edges {
+              node {
+                id
+                name
+                pullRequests(first:10) {
+                  edges {
+                    node {
+                      id
+                      title
+                    }
+                  }
                 }
               }
             }
           }
         }
-      }
-    }
-  }`;
+      }`;
 
-  req.write(JSON.stringify({query}));
-  req.end();
+      req.write(JSON.stringify({query}));
+      req.end();
+    });
+  }
+}
+
+async function main() {
+  try {
+    let github = new Github(config.github_token);
+    let prs = await github.getPRs(config.github_organization);
+    console.log(JSON.stringify(prs, null, 4));
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 main();
